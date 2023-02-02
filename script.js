ee82a196987d7a1fcf9a6285b7cc81151b71726b
Minh Le <beaver700nh@gmail.com>
2023-02-01 22:18:50 -0500

    more selection

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const WIDTH = 23;
 const HEIGHT = 19;
 
+function main() {
+  createCells(WIDTH, HEIGHT);
+}
+
+function createCells(width, height) {
+  const cellTemplate = document.getElementById("--cell");
+  const cellContainer = document.getElementsByClassName("cell-container")[0];
+
+  const selectionTemplate = document.getElementById("--selection");
+  const selectionContainer = document.getElementsByClassName("selection-list")[0];
+
+  cellContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+
+  for (let j = 0; j < height; ++j) {
+    for (let i = 0; i < width; ++i) {
+      let cell = spawnFromTemplate(cellTemplate, cellContainer);
+
+      cell.addEventListener("click", onCellClick);
+      cell.dataset.index = j*width + i;
+
+      if (j % 2 !== 0) {
+        cell.classList.add("odd-row");
+      }
+      if (j % 2 !== 0 && i + 1 === width) {
+        cell.classList.add("leftover");
+      }
+
+      let selection = spawnFromTemplate(selectionTemplate, selectionContainer);
+      selection.textContent = `${i + 1}.${j + 1}`;
+    }
+  }
+}
+
 function spawnFromTemplate(template, container) {
   const clone = template.content.cloneNode(true);
   const elem = clone.children[0];
@@ -8,31 +41,25 @@ function spawnFromTemplate(template, container) {
   return elem;
 }
 
-function main() {
-  const cellTemplate = document.getElementById("--cell");
-  const cellContainer = document.getElementsByClassName("cell-container")[0];
+function onCellClick(event) {
+  let cell = event.target;
+  cell.classList.toggle("selected");
 
-  for (let j = 0; j < HEIGHT; ++j) {
-    for (let i = 0; i < WIDTH; ++i) {
-      let cell = spawnFromTemplate(cellTemplate, cellContainer);
-
-      cell.addEventListener("click", onCellClick);
-
-      if (j % 2 !== 0) {
-        cell.classList.add("odd-row");
-      }
-      if (j % 2 !== 0 && i + 1 === WIDTH) {
-        cell.classList.add("leftover");
-      }
-    }
-  }
-
-  cellContainer.style.gridTemplateColumns = `repeat(${WIDTH}, 1fr)`;
+  const index = cell.dataset.index;
+  let selection = document.getElementsByClassName("selection-list")[0].children[index];
+  selection.classList.toggle("hidden");
 }
 
-function onCellClick(event) {
-  const cell = event.target;
-  cell.classList.toggle("selected");
+function clearSelection() {
+  let selected = document.querySelectorAll(".cell.selected");
+  let selections = document.getElementsByClassName("selection-list")[0];
+
+  for (let cell of selected) {
+    cell.classList.remove("selected");
+
+    const index = cell.dataset.index;
+    selections.children[index].classList.add("hidden");
+  }
 }
 
 document.addEventListener("DOMContentLoaded", main);
