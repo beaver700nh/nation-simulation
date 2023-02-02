f2407bebc34872bee4c14eac448de02f9d068de6
Minh Le <beaver700nh@gmail.com>
2023-02-01 21:12:36 -0500

    cell selection works

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-document.addEventListener("DOMContentLoaded", main);
-
-function main() {
-  const cellTemplate = document.getElementById("--cell");
-  const cellContainer = document.getElementsByClassName("cell-container")[0];
-
-  for (let j = 0; j < 19; ++j) {
-    for (let i = 0; i < 23; ++i) {
-      let cell = spawnFromTemplate(cellTemplate, cellContainer);
-
-      if (j % 2 !== 0) {
-        cell.classList.add("odd-row");
-      }
-      if (j % 2 !== 0 && i === 22) {
-        cell.classList.add("leftover");
-      }
-    }
-  }
-
-  cellContainer.style.gridTemplateColumns = `repeat(23, 1fr)`;
-}
+const WIDTH = 23;
+const HEIGHT = 19;
 
 function spawnFromTemplate(template, container) {
   const clone = template.content.cloneNode(true);
@@ -26,3 +7,32 @@ function spawnFromTemplate(template, container) {
   container.appendChild(elem);
   return elem;
 }
+
+function main() {
+  const cellTemplate = document.getElementById("--cell");
+  const cellContainer = document.getElementsByClassName("cell-container")[0];
+
+  for (let j = 0; j < HEIGHT; ++j) {
+    for (let i = 0; i < WIDTH; ++i) {
+      let cell = spawnFromTemplate(cellTemplate, cellContainer);
+
+      cell.addEventListener("click", onCellClick);
+
+      if (j % 2 !== 0) {
+        cell.classList.add("odd-row");
+      }
+      if (j % 2 !== 0 && i + 1 === WIDTH) {
+        cell.classList.add("leftover");
+      }
+    }
+  }
+
+  cellContainer.style.gridTemplateColumns = `repeat(${WIDTH}, 1fr)`;
+}
+
+function onCellClick(event) {
+  const cell = event.target;
+  cell.classList.toggle("selected");
+}
+
+document.addEventListener("DOMContentLoaded", main);
